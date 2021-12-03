29a6b14920356abc845077234e345ce019900bd1
Kenn Stack <kibadikennedy@gmail.com>
2021-12-03 21:49:26 +0800

    hmmmm

--- src/components/Realisations/Realisation.js
+++ src/components/Realisations/Realisation.js
@@ -3,14 +3,17 @@ import { Link } from "gatsby"
 
 const Realisation = ({ imageSrc, title, description, slug }) => {
   return (
-    <Link to={`/realisations/${slug}`} className="mx-1 md:mx-2 my-6 relative ">
+    <Link
+      to={`/realisations/${slug}`}
+      className="mx-1 md:mx-2 my-6 relative   transform transition duration-500 hover:scale-105"
+    >
       <img
         src={imageSrc}
         loading="lazy"
         placeholder="blurred"
         className="mx-auto rounded md:w-4/5 relative inset-0 bg-cover bg-center z-0 filter brightness-50 "
       />
-      <div className="absolute flex flex-col text-center mx-auto mt-14 inset-0 justify-center items-center text-white font-semibold z-10  transform transition duration-500 hover:scale-110">
+      <div className="absolute flex flex-col text-center mx-auto mt-14 inset-0 justify-center items-center text-white font-semibold z-10">
         <h1 className=" text-xl md:text-3xl">{title}</h1>
         <p className="text-md my-2 "> {description} </p>
       </div>
