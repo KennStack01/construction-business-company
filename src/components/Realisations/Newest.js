b09e5eafc937e3097c2a1382385bb4d85d61eb44
Kenn Stack <kibadikennedy@gmail.com>
2021-12-11 00:04:48 +0800

    added changed the link for the slug

--- src/components/Realisations/Newest.js
+++ src/components/Realisations/Newest.js
@@ -13,7 +13,7 @@ const Newest = ({ imageSrc, title, slug, description }) => {
         className="bg-cover rounded-lg md:w-1/2"
       />
       <div className="flex flex-col justify-start my-auto md:w-1/2 md:mx-4">
-        <Link to={`/blog/${slug}`}>
+        <Link to={`/realisations/${slug}`}>
           <div>
             <h1 className="text-xl md:text-3xl font-bold hover:text-sadi-red-500">
               {" "}
